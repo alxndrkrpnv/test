ac46288b05e3a73a5e4e1533b80183cbc1229efe
alxndrkrpnv <113288998+alxndrkrpnv@users.noreply.github.com>
2022-09-12 14:34:03 +0400

    Add files via upload

--- task2.py
+++ task2.py
@@ -1,11 +1,12 @@
+import sys
+
 class MinStepsToOneNumber:
     def steps(self):
         def read_from_file():
+            filepath = sys.argv[1]
             l = []
-            path = r'f1.txt'
-            with open(path, 'r', encoding='utf-8') as f:
+            with open(filepath, 'r', encoding='utf-8') as f:
                     l = [int(number) for number in f.readline().split(' ')]
-                    print(l)
                     return l
 
         nums = read_from_file()
